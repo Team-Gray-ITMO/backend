632675cc0ba1e7bf8660ebe392174271d46f4387
Fedor <lagutinfedya@gmail.com>
2024-11-18 19:20:24 +0300

    feat: add CRUD for skill

---
+++ backend/src/main/java/vk/itmo/teamgray/backend/skill/dto/SkillCreateDto.java
@@ -0,0 +1,15 @@
+package vk.itmo.teamgray.backend.skill.dto;
+
+import vk.itmo.teamgray.backend.skill.enums.SkillProficiency;
+
+import java.io.Serializable;
+
+/**
+ * DTO for {@link vk.itmo.teamgray.backend.skill.entities.Skill}
+ */
+public record SkillCreateDto(
+        Long resumeId,
+        String name,
+        SkillProficiency proficiency
+) implements Serializable {
+}
---
+++ backend/src/main/java/vk/itmo/teamgray/backend/skill/services/SkillService.java
@@ -0,0 +1,39 @@
+package vk.itmo.teamgray.backend.skill.services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import vk.itmo.teamgray.backend.common.exceptions.ModelNotFoundException;
+import vk.itmo.teamgray.backend.resume.services.ResumeService;
+import vk.itmo.teamgray.backend.skill.dto.SkillCreateDto;
+import vk.itmo.teamgray.backend.skill.dto.SkillUpdateDto;
+import vk.itmo.teamgray.backend.skill.entities.Skill;
+import vk.itmo.teamgray.backend.skill.repos.SkillRepository;
+
+@Service
+@RequiredArgsConstructor
+public class SkillService {
+    private final SkillRepository skillRepository;
+    private final ResumeService resumeService;
+
+    public Skill findById(Long id){
+        return skillRepository.findById(id).orElseThrow(ModelNotFoundException::new);
+    }
+
+    public Skill createSkill(SkillCreateDto data){
+        return skillRepository.save(new Skill(
+                data,
+                resumeService.findById(data.resumeId())
+        ));
+    }
+
+    public Skill updateSkill(SkillUpdateDto data){
+        return skillRepository.save(new Skill(
+                data,
+                resumeService.findById(data.resumeId())
+        ));
+    }
+
+    public void deleteById(Long id){
+        skillRepository.deleteById(id);
+    }
+}
