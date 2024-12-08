1db58a19a415866793902900f8fe94ce1b644b02
Fedor <lagutinfedya@gmail.com>
2024-12-08 18:32:42 +0300

    feat: add method for convertion to PDF

--- backend/src/main/java/vk/itmo/teamgray/backend/resume/services/ResumeExportService.java
+++ backend/src/main/java/vk/itmo/teamgray/backend/resume/services/ResumeExportService.java
@@ -1,10 +1,17 @@
 package vk.itmo.teamgray.backend.resume.services;
 
+import com.itextpdf.html2pdf.HtmlConverter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import vk.itmo.teamgray.backend.resume.entities.Resume;
+import vk.itmo.teamgray.backend.resume.exceptions.ConvertionException;
 import vk.itmo.teamgray.backend.template.services.TemplateMergeService;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 @Service
 @RequiredArgsConstructor
@@ -14,6 +21,24 @@ public class ResumeExportService {
     private final TemplateMergeService templateMergeService;
 
     public byte[] extractHtml(Long resumeId) {
+        Resume resume = resumeService.findById(resumeId);
+        return templateMergeService.mergeTemplateToHtml(resume);
+    }
+
+    public byte[] extractPdf(Long resumeId) {
+        try {
+            Resume resume = resumeService.findById(resumeId);
+            var htmlTemplate = templateMergeService.mergeTemplateToHtml(resume);
+            ByteArrayOutputStream pdfOutputStream = new ByteArrayOutputStream();
+            HtmlConverter.convertToPdf(new ByteArrayInputStream(htmlTemplate), pdfOutputStream);
+            return pdfOutputStream.toByteArray();
+        }
+        catch (IOException e){
+            throw new ConvertionException("ERROR.CONVERT_TO_PDF: " + e.getMessage());
+        }
+    }
+
+    public byte[] extractDocx(Long resumeId) {
         Resume resume = resumeService.findById(resumeId);
         return templateMergeService.mergeTemplateToHtml(resume);
     }
