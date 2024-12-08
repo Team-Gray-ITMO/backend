0b8a9fa881b4fccff0be61b0755cf539bc760bd4
Boris Ivanov <bors2908@gmail.com>
2024-12-08 17:11:29 +0300

    VKM-43: Add Swagger generation

--- backend/src/main/java/vk/itmo/teamgray/backend/config/ApplicationConfiguration.java
+++ backend/src/main/java/vk/itmo/teamgray/backend/config/ApplicationConfiguration.java
@@ -1,4 +1,4 @@
-package vk.itmo.teamgray.backend;
+package vk.itmo.teamgray.backend.config;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
