0bbb2524dc6d158c6fbee6ac6c78834383809547
Vladimir Lidzhigoryaev <vlidzhigoryaev@digital-spirit.ru>
2024-12-17 13:37:06 +0300

    VKM-58: add endpoints needed by front-end

--- src/main/java/vk/itmo/teamgray/backend/common/config/SecurityConfig.java
+++ src/main/java/vk/itmo/teamgray/backend/common/config/SecurityConfig.java
@@ -31,10 +31,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        //configuration.setAllowedOrigins(List.of(lkPath));
+        configuration.setAllowedOrigins(List.of("*"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(List.of("*"));
-        configuration.setAllowCredentials(true);
+        configuration.setAllowCredentials(false);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
