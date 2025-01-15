d645aa77f069c39f3eaba971a23ec39235320791
Fedor <lagutinfedya@gmail.com>
2025-01-15 21:27:37 +0300

    feat: add company data validation

--- src/main/java/vk/itmo/teamgray/backend/company/dto/CompanyCreateDto.java
+++ src/main/java/vk/itmo/teamgray/backend/company/dto/CompanyCreateDto.java
@@ -1,6 +1,9 @@
 package vk.itmo.teamgray.backend.company.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,8 +14,12 @@ import lombok.NoArgsConstructor;
 @Data
 public class CompanyCreateDto {
     @Schema(description = "Name of the company")
+    @NotNull
+    @NotEmpty
+    @Size(max = 255)
     private String name;
 
     @Schema(description = "Company URL")
+    @Size(max = 255)
     private String url;
 }
--- src/main/java/vk/itmo/teamgray/backend/company/dto/CompanyUpdateDto.java
+++ src/main/java/vk/itmo/teamgray/backend/company/dto/CompanyUpdateDto.java
@@ -1,6 +1,9 @@
 package vk.itmo.teamgray.backend.company.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -12,8 +15,12 @@ public class CompanyUpdateDto {
     private long id;
 
     @Schema(description = "Name of the company")
+    @NotNull
+    @NotEmpty
+    @Size(max = 255)
     private String name;
 
     @Schema(description = "Company URL")
+    @Size(max = 255)
     private String url;
 }
